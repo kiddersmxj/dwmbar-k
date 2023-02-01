d68472280c4a73978a88cdae6076aa62774066d1
kiddersmxj <maxkiddmay@gmail.com>
2023-02-01 16:23:03 +0000

    added get volume level functionality if no args are passed

--- modules/volume-k.cpp
+++ modules/volume-k.cpp
@@ -4,6 +4,8 @@
 #include <alsa/asoundlib.h>
 #include "../include/dwmbar-k.h" 
 
+const snd_mixer_selem_channel_id_t CHANNEL = SND_MIXER_SCHN_FRONT_LEFT;
+
 static void error_close_exit(const char *errmsg, int err, snd_mixer_t *h_mixer) {
 	if (err == 0)
 		fprintf(stderr, errmsg);
@@ -23,13 +25,37 @@ int main(int argc, char** argv) {
 	char *device;
 	char *selem_name;
 
+	device = "default";
+	selem_name = std::getenv("scontrol");
+
 	if (argc < 2) {
-		printf("Usage:\n\t%s device control volume\n", argv[0]);
+	    if ((err = snd_mixer_open(&h_mixer, 1)) < 0)
+    		error_close_exit("Mixer open error: %s\n", err, NULL);
+    
+    	if ((err = snd_mixer_attach(h_mixer, device)) < 0)
+    		error_close_exit("Mixer attach error: %s\n", err, h_mixer);
+    
+    	if ((err = snd_mixer_selem_register(h_mixer, NULL, NULL)) < 0)
+    		error_close_exit("Mixer simple element register error: %s\n", err, h_mixer);
+    
+    	if ((err = snd_mixer_load(h_mixer)) < 0)
+    		error_close_exit("Mixer load error: %s\n", err, h_mixer);
+    
+    	snd_mixer_selem_id_alloca(&sid);
+    	snd_mixer_selem_id_set_index(sid, 0);
+    	snd_mixer_selem_id_set_name(sid, selem_name);
+    
+    	if ((elem = snd_mixer_find_selem(h_mixer, sid)) == NULL)
+    		error_close_exit("Cannot find simple element\n", 0, h_mixer);
+    
+    	snd_mixer_selem_get_playback_volume(elem, CHANNEL, &vol);
+    	printf("%ld\n", vol);
+    
+    	snd_mixer_close(h_mixer);
+
 		return 0;
 	}
 
-	device = "default";
-	selem_name = std::getenv("scontrol");
 	vol = atol(argv[1]);
 
 	if ((err = snd_mixer_open(&h_mixer, 1)) < 0)
