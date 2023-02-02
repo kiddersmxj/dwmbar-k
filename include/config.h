9d9ba43ce71722ee0d0f85093b77fd4b5ddbcfe8
kiddersmxj <maxkiddmay@gmail.com>
2023-02-02 22:48:44 +0000

    added `const std::string ModuleLayout` to set module position and order in bar

--- include/config.h
+++ include/config.h
@@ -3,6 +3,7 @@
 
 const int ModulesLength = 5;
 const std::string Modules[ModulesLength] = { "network-k", "time-k", "weather-k", "volume-k", "media-k" };
+const std::string ModuleLayout = "1,2,;,3,4,5";
 
 const float VolScaler = 0.865;
 
