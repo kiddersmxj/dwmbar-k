4471c907fb3f4d2f7839cd9cc30e2930769ebcb5
kiddersmxj <maxkiddmay@gmail.com>
2023-02-02 14:57:55 +0000

    weather-k fwv, curls `wttr.in` for weather info, has timeout without proper error handling - TODO. prints hex string after parsing inedible values for `xsetroot -name`

--- modules/weather-k.cpp
+++ modules/weather-k.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <cstring>
 #include "../include/dwmbar-k.h" 
 
 int main() {
-    
-    return 1;
+    const char* CurlCmd = R"(timeout 1 curl -s wttr.in/$LOCATION?format=1 | hexdump -v -e '"\\\x" 1/1 "%02x"')";
+    std::string WttrHex = ExecCmd(CurlCmd, 0, 0);
+    if(WttrHex == "")
+        std::cout << "curl did not exec" << std::endl;
+
+    // Allocating space for array, +1 ro allow for NULL term
+    char* HexArray = new char[WttrHex.length() + 1];
+    strcpy(HexArray, WttrHex.c_str());
+    char* substr = strtok(HexArray, R"(\)");
+
+    int i = 0;
+    int space = 0;
+    std::string HexOutput = "\\";
+    while(substr != NULL) {
+        if(i < 4 && strcmp(substr, "xef") != 0) {
+            HexOutput =+ " \\";
+            HexOutput =+ substr;
+            std::cout << "\\" << substr;
+            /* std::cout << "q"; */
+        } else if(strcmp(substr, "x2b") == 0) {
+        } else if(strcmp(substr, "x20") == 0 && space == 0) {
+            space++;
+            /* std::cout << "w"; */
+        } else if(space == 1) {
+            HexOutput =+"\\";
+            HexOutput =+ substr;
+            std::cout << "\\" << substr;
+        }
+        substr = strtok(NULL, R"(\)");
+        i++;
+    }
+    delete[] HexArray;
+
+    return 0;
 }
 
