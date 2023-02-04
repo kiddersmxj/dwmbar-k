3ebc98240ce9b54b73f8d172477d7ab29a8a1059
kiddersmxj <maxkiddmay@gmail.com>
2023-02-04 19:35:51 +0000

    added `battery-k` to const std::string Modules[]`

--- include/config.h
+++ include/config.h
@@ -1,8 +1,8 @@
 #ifndef config.h 
 #define config.h 
 
-const int ModulesLength = 5;
-const std::string Modules[ModulesLength] = { "network-k", "time-k", "weather-k", "volume-k", "media-k" };
+const int ModulesLength = 6;
+const std::string Modules[ModulesLength] = { "network-k", "time-k", "weather-k", "volume-k", "media-k", "battery-k"};
 const std::string ModuleLayout = "1,2,;,3,4,5";
 
 const float VolScaler = 0.865;
