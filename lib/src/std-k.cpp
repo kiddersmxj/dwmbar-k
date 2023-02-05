596ed3e70f7e413821c811cbd599abd8e2a89076
kiddersmxj <maxkiddmay@gmail.com>
2023-02-05 22:47:45 +0000

    added assortment of general use functions and their header defenitions

--- lib/src/std-k.cpp
+++ lib/src/std-k.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <fstream>
 #include <array>
+#include "../std-k.hpp"
 
-std::string ExecCommand(const std::string cmd, const int Output, int ExitStatus) {
+// NULL function to attach breakpoint to in gdb
+void BreakPoint(void) {
+    std::cout << "";
+}
+
+std::string ExecCmd(const std::string cmd, const int Output, int ExitStatus) {
 	auto pPipe = ::popen(cmd.c_str(), "r");
 	if(pPipe == nullptr)
 	{
@@ -16,16 +23,61 @@ std::string ExecCommand(const std::string cmd, const int Output, int ExitStatus)
 	    auto bytes = fread(buffer.data(), 1, buffer.size(), pPipe);
 	    result.append(buffer.data(), bytes);
 	}
-	
+
 	auto rc = ::pclose(pPipe);
 
     if(Output == 1)
         std::cout << result << std::endl;
 
-	if(WIFEXITED(rc)) {
+    if(WIFEXITED(rc)) {
 	    ExitStatus = WEXITSTATUS(rc);
 	}
 
 	return result;
+}
+
+void VPrint(std::vector<std::string> Input) {
+	for(std::string Output: Input)
+	    std::cout << Output << std::endl;
+}
+
+void WriteFileLines(std::vector<std::string> Lines, std::string File) {
+    std::fstream FileToWrite(File, std::ios::out);
+	if (!FileToWrite) {
+        std::cout << "File not created!" << std::endl;
+	}
+	else {
+        for(std::string Line: Lines)
+            FileToWrite << Line << std::endl;
+		FileToWrite.close();
+	}
+}
+
+std::vector<std::string> ReadFileLines(std::string File) {
+    std::string Line;
+    std::vector<std::string> Output;
+    std::ifstream FileToRead(File);
+
+    while(getline(FileToRead, Line)) {
+        Output.push_back(Line);
+    }
+
+    FileToRead.close();
+    return Output;
+}
+
+// Pass delim most likley usng string literal R"(...)"
+std::vector<std::string> SplitStr(std::string String, const char* Delim) {
+    char* Array = new char[String.length() + 1];
+    strcpy(Array, String.c_str());
+    char* substr = strtok(Array, Delim);
+    
+    std::vector<std::string> Output;
+    while(substr != NULL) {
+        Output.push_back(substr);
+    }
+    delete[] Array;
+
+    return Output;
 }
 
--- lib/std-k.hpp
+++ lib/std-k.hpp
@@ -1,6 +1,14 @@
 #ifndef Kstd
 #define Kstd
 
+#include <vector>
+#include <cstring>
+
+void BreakPoint();
 std::string ExecCmd(const std::string cmd, const int Output, int ExitStatus);
+void WriteFileLines(std::vector<std::string> Lines, std::string File);
+std::vector<std::string> ReadFileLines(std::string File);
+void VPrint(std::vector<std::string> Input);
+std::vector<std::string> SplitStr(std::string String, const char* Delim);
 
 #endif
