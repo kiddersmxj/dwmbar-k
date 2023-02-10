f834d1523112a80cc10ec0622913b2254d1d36c3
kiddersmxj <maxkiddmay@gmail.com>
2023-02-10 15:44:42 +0000

    began moving lib includes over

--- include/dwmbar-k.hpp
+++ include/dwmbar-k.hpp
@@ -2,9 +2,13 @@
 #define Kdwmbar
 
 #include <vector>
+#include <chrono>
+#include <thread>
 
 #include "config.hpp"
 #include "../lib/std-k.hpp"
+
+using namespace std::chrono_literals;
 
 #endif 
 
