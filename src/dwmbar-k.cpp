3a791f2d0024cbcd4a282a6e11915047ddd7bad8
kiddersmxj <maxkiddmay@gmail.com>
2023-02-10 09:38:43 +0000

    `void InitClock()` now resets clock file to zero using `PollClock(...)` at the beginning of main

--- src/dwmbar-k.cpp
+++ src/dwmbar-k.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <filesystem>
 #include <cstring>
 #include "../include/dwmbar-k.hpp"
+namespace fs = std::filesystem;
 
 void RunModule(std::string Module) {
     std::cout << Module << std::endl;
@@ -23,11 +25,22 @@ std::string ParseModuleNo(std::string ModuleNo) {
 }
 
 void InitClock() {
-    PollClock(CDir);
+#ifdef COUT
+    std::cout << "Clock=" << PollClock(CDir);
+#endif
+    if(PollClock(CDir) != 0) {
+        for (const auto & entry : fs::directory_iterator(CDir)) {
+	        rename(entry.path(), CDir + "/0");
+        }
+    }
+#ifdef COUT
+    std::cout << "...Clock=" << PollClock(CDir) << std::endl;
+#endif
 }
 
 int main() {
     InitClock();
+    std::cout << PollClock(CDir) << std::endl;
     std::string result = ExecCmd(R"(mkdir $HOME/devel/dwmbar-k/.tmp > /dev/null 2>&1 || echo 1)", 0, 0);
     std::cout << "CMD-" << result << "-" << std::endl;
     if(result == "") {
