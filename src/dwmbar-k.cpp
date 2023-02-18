0480058246d4a703601e824089fe0bb25ea72bcd
kiddersmxj <maxkiddmay@gmail.com>
2023-02-18 11:13:10 +0000

    bar parses full output and uses `std::string Backup[ModulesLength + 1` to rewrite `std::string Output[ModulesLength + 1]` if the output file is blank for whatever reason

--- src/dwmbar-k.cpp
+++ src/dwmbar-k.cpp
@@ -3,6 +3,8 @@
 #include <numeric> 
 #include <fstream>
 #include <filesystem>
+#include <string>
+#include <vector>
 #include "../include/dwmbar-k.hpp"
 namespace fs = std::filesystem;
 
@@ -35,6 +37,7 @@ std::string GetModuleOutput(std::string Module);
 std::string ParseModuleNo(std::string ModuleNo);
 void XSR(std::string Body);
 std::string ParseXSR(std::vector<std::string> VectorOutput);
+int GetIndex(std::vector<std::string> v, std::string K);
 
 int main() {
     KillModules();
@@ -45,36 +48,45 @@ int main() {
 
 	class InternalClock Internal;
     
-    std::vector<std::string> Output;
+    // Plus one to allow for BarDelimeter
+    std::string Output[ModulesLength + 1];
+    std::string Backup[ModulesLength + 1];
 
-    // Only runs if .bashrc set $dwmbar to 1
-    while(getenv("dwmbar")) {
+    while(1) {
         RunModules();
         if(Internal.Get() % ClockFrq == 0) {
+            int i = 0;
 	    	for(std::string substr: ModuleLayout) {
-                if(substr == ";") {
-					Output.push_back(";");
+                if(substr == BarDelimeter) {
+                    Output[i] = BarDelimeter;
+                    Backup[i] = BarDelimeter;
 #ifdef COUT
-                    std::cout << ";" << std::endl;
+                    std::cout << BarDelimeter << std::endl;
 #endif
                 } else {
-					std::string Out = GetModuleOutput(ParseModuleNo(substr));
-					// Make sure not parsing empty module data
-					if(Out != "")
-						Output.push_back(Out);
+                    std::string Out = GetModuleOutput(ParseModuleNo(substr));
+                    if(Out != "") {
+                        Output[i] = Out;
+                        Backup[i] = Out;
+                    } else {
+                        Output[i] = Backup[i];
+                        // TODO: Make a warning if Output is missed too many times
+                    }
                 }
+                i++;
 		    }
 #ifdef OCOUT
 			std::cout << "-" << std::endl;
-			VPrint(Output);
+            for(int i = 0; i<(ModulesLength + 1); i++)
+                std::cout << Output[i] << std::endl;
 			std::cout << "-" << std::endl;
 #endif
 			
-			XSR(ParseXSR(Output));
+            std::vector<std::string> OutputVector(Output, Output + (ModulesLength + 1));
+			XSR(ParseXSR(OutputVector));
 
 		    BreakPoint();
             Clock.Pulse();
-            Output.clear();
         }
         Internal.Pulse();
     }
@@ -102,10 +114,8 @@ void Clock::Init() {
 #endif
     if(PollClock(CDir) == -1) {
         Touch(CDir + "/0");
-    } else if(PollClock(CDir) != 0) {
-        for (const auto & entry : fs::directory_iterator(CDir))
-	        rename(entry.path(), CDir + "/0");
-    }
+    } else if(PollClock(CDir) != 0)
+        Reset();
 #ifdef COUT
     std::cout << "...Clock=" << PollClock(CDir) << std::endl;
 #endif
@@ -116,13 +126,22 @@ void Clock::Pulse() {
     std::cout << "Clock=" << PollClock(CDir);
 #endif
     int Pulse = PollClock(CDir);
-    Pulse++;
-    for (const auto & entry : fs::directory_iterator(CDir))
-	    rename(entry.path(), CDir + "/" + std::to_string(Pulse));
+    if(Pulse == MaxClock)
+        Reset();
+    else {
+        Pulse++;
+        for (const auto & entry : fs::directory_iterator(CDir))
+	        rename(entry.path(), CDir + "/" + std::to_string(Pulse));
+    }
 #ifdef COUT
     std::cout << "...Clock=" << PollClock(CDir) << std::endl;
 #endif
 } 
+
+void Clock::Reset() {
+        for (const auto & entry : fs::directory_iterator(CDir))
+	        rename(entry.path(), CDir + "/0");
+}
 
 void RunModule(std::string Module) {
 #ifdef COUT
@@ -197,10 +216,19 @@ void XSR(std::string Body) {
     ExecCmd(Cmd, 0, 0);
 }
 
-std::string ParseXSR(std::vector<std::string> VectorOutput) {
+std::string ParseXSR(std::vector<std::string> OutputVector) {
 	std::string XSRBody = "";
-	for( std::string ModOutput: VectorOutput)
-		XSRBody += " " + BarDelimeter + " " + ModOutput;
+    // Make sure previous Output was not empty or BarDelimeter
+    int Escape = 0;
+    for(std::string Output: OutputVector) {
+        if(Output != BarDelimeter && Output != "" && Escape == 0)
+            XSRBody += " " + ModuleDelimeter;
+        else if(Escape)
+            Escape = 0;
+        else
+            Escape = 1;
+		XSRBody += " " + Output;
+    }
 	std::cout << XSRBody << std::endl;
 	return XSRBody;
 }
