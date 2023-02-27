8fb5aa11ff8524c83d6a1b58ca45c0a2e8b4b6a7
kiddersmxj <maxkiddmay@gmail.com>
2023-02-27 22:10:58 +0000

    finished `std::string GetPlayer()` to return current player or paused or overaload depending on media stats

--- modules/media-k.cpp
+++ modules/media-k.cpp
@@ -1,6 +1,8 @@
 #include "../include/dwmbar-k.hpp" 
 #include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
 
 std::vector<std::string> SplitString(const std::string& str) {
     std::vector<std::string> tokens;
@@ -30,36 +32,41 @@ std::vector<std::string> GetPlayers() {
 std::string GetPlayer() {
     int i = 0;
     std::string Player;
-	for(std::string P: GetPlayers()) {
+	std::vector<std::string> Players = GetPlayers();
+	for(std::string P: Players) {
+#ifdef MCOUT
+        std::cout << "P-" << P << "-" << std::endl;
+#endif
         std::string Output = ExecCmd(R"(playerctl -p )" + P + R"( status)", 0, 0);
         Output.erase(std::remove(Output.begin(), Output.end(), '\n'), Output.cend());
 #ifdef MCOUT
-        std::cout << "-" << Output << "-" << std::endl;
+        std::cout << "out-" << Output << "-" << std::endl;
 #endif
         if(Output == "Playing") {
 #ifdef MCOUT
-            std::cout << "-" << P << "-" << std::endl;
+            std::cout << "Playing-" << P << "-" << std::endl;
 #endif
             i++;
             Player = P;
 		}
-		switch(i) {
-			case 0:
-				return "Paused";
-			case 1:
-				return Player;
-			case 2:
-				return "Overloaded";
-			default:
-				return "Error";
-		}
+	}
+	switch(i) {
+		case 0:
+			return "Paused";
+		case 1:
+			return Player;
+		case 2:
+			return "Overloaded";
+		default:
+			return "Error";
 	}
 	return "None";
 }
 
 void Media() {
     std::string Player = GetPlayer();
-    std::cout << Player << std::endl;
+	// TODO Add overload to pause old media when new media is played (possibly play again when new media is paused)
+    std::cout << "Player=" + Player << std::endl << std::endl;
 }
 
 int main() {
