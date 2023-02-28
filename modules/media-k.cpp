660204df0a70bbc464d7d8824bbd9d8d9499e688
kiddersmxj <maxkiddmay@gmail.com>
2023-02-28 14:33:40 +0000

    added abilities to get artist and title and put into output file

--- modules/media-k.cpp
+++ modules/media-k.cpp
@@ -4,6 +4,22 @@
 #include <string>
 #include <vector>
 
+int C = -1;
+
+struct {
+    std::string Player, Hex, Artist, Title, Time;
+} media;
+
+int Run() {
+    if(C == PollClock(CDir)) {
+        return 0;
+    }
+    C = PollClock(CDir);
+    if((PollClock(CDir) % MediaFrq) == 0 || PollClock(CDir) == 0)
+        return 1;
+    return 0; 
+}
+
 std::vector<std::string> SplitString(const std::string& str) {
     std::vector<std::string> tokens;
  
@@ -37,8 +53,7 @@ std::string GetPlayer() {
 #ifdef MCOUT
         std::cout << "P-" << P << "-" << std::endl;
 #endif
-        std::string Output = ExecCmd(R"(playerctl -p )" + P + R"( status)", 0, 0);
-        Output.erase(std::remove(Output.begin(), Output.end(), '\n'), Output.cend());
+        std::string Output = StripTrailingNL(ExecCmd(R"(playerctl -p )" + P + R"( status)", 0, 0));
 #ifdef MCOUT
         std::cout << "out-" << Output << "-" << std::endl;
 #endif
@@ -63,15 +78,47 @@ std::string GetPlayer() {
 	return "None";
 }
 
+std::string GetArtist(std::string Player) {
+    return StripTrailingNL(ExecCmd(R"(playerctl --player=)" + Player + R"( metadata --format '{{ artist }}')", 0 ,0));
+}
+
+std::string GetTitle(std::string Player) {
+    return StripTrailingNL(ExecCmd(R"(playerctl --player=)" + Player + R"( metadata --format '{{ title }}')", 0, 0));
+}
+
 void Media() {
-    std::string Player = GetPlayer();
-	// TODO Add overload to pause old media when new media is played (possibly play again when new media is paused)
-    std::cout << "Player=" + Player << std::endl << std::endl;
+    if(!Run())
+        return;
+    std::cout << std::endl;
+    media.Player = GetPlayer();
+	// TODO Add overload to pause old media when new media is played (possibly play again when new media is paused) - togglable?
+    if(media.Player != "Paused") {
+        media.Hex = IPlay;
+        media.Artist = GetArtist(media.Player);
+        media.Title = GetTitle(media.Player);
+    } else if (media.Player == "Overload") {
+        // TODO overload error
+    } else {
+        media.Hex = IPause;
+    }
+
+#ifdef MCOUT
+    std::cout << R"(Player=")" << media.Player << R"(")" << std::endl;
+    std::cout << R"(Artist=")" << media.Artist << R"(")" << std::endl;
+    std::cout << R"(Title=")" << media.Title << R"(")" << std::endl << std::endl;
+#endif
+
+    std::string Out = media.Hex + " " + media.Artist + " - " + media.Title;
+
+    std::vector<std::string> Output;
+    Output.push_back(R"($(printf ")" + Out + R"("))");
+    WriteFileLines(Output, MediaOutputFile);
 }
 
 int main() {
     while(1) {
         Media();
+        BreakPoint();
     }
     return 0;
 }
