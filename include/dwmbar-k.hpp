99b1a94ef02dc085d956a795fa668ce91e661dc0
kiddersmxj <maxkiddmay@gmail.com>
2023-06-28 18:46:29 +0100

    added PollClock as an inline function

--- include/dwmbar-k.hpp
+++ include/dwmbar-k.hpp
@@ -7,6 +7,21 @@
 
 #include "config.hpp"
 
+inline int PollClock(const std::string Dir) {
+    std::string File;
+    int FileInt;
+    for (const auto & entry : std::filesystem::directory_iterator(Dir))
+        File = std::filesystem::path(entry.path()).filename();
+    if(File == "")
+        FileInt = -1;
+    else
+        FileInt = stoi(File);
+#ifdef COUT
+    std::cout << FileInt << std::endl;
+#endif
+    return FileInt;
+}
+
 using namespace std::chrono_literals;
 
 #endif 
